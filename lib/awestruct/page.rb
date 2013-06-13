d3e8c77dcb2e8c560a786da8d4995fc1d7c43c3e
LightGuard <lightguard.jp@gmail.com>
2013-06-13 16:55:33 -0600

    Slowly working through tests as I create them

--- lib/awestruct/page.rb
+++ lib/awestruct/page.rb
@@ -3,18 +3,53 @@ module Awestruct
     attr_reader :dependencies
     attr_reader :layout
     attr_reader :front_matter
+    attr_reader :relative_source_path
+    attr_reader :output_path
+    attr_reader :output_filename
+    attr_reader :source_path
+    attr_reader :raw_content
+    attr_reader :rendered_content
+    attr_reader :content
 
     def initialize(file, site)
       @front_matter = []
-      parse_front_matter
+      @raw_content = []
+
+      read_file file, site
     end
 
     def render()
     end
 
-    private
-    def parse_front_matter()
+    def ==
 
+    end
+
+    private
+    def read_file(resource, site)
+      # TODO: read each line, separate out the front matter, everything else becomes raw_content
+      if (File.exist?(File.join(site.config.dir, resource)) && File.file?(File.join(site.config.dir, resource)))
+        resource_content = File.open(File.join(site.config.dir, resource)).readlines
+        @source_path = Pathname.new(File.join(site.config.dir, resource)).relative_path_from(Pathname.new site.config.dir).to_s
+      else # Assume resource is a string
+        resource_content = resource.split(/\n/)
+      end
+
+      in_front_matter = false
+      resource_content.each do |line|
+        if /^---$/ =~ line
+          in_front_matter = !in_front_matter
+          next
+        end
+        if in_front_matter
+          @front_matter << line
+        else
+          @raw_content << line
+        end
+        # TODO: parse front matter
+        @raw_content = @raw_content.join
+        @front_matter = YAML.load @front_matter.join
+      end
     end
   end
 end
--- spec/awestruct/page_spec.rb
+++ spec/awestruct/page_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
+
 require 'awestruct/page'
 
 describe 'Awestruct::Page' do
+  before(:each) do
+    @site = double()
+    @site.stub_chain(:config, :dir).and_return('spec')
+  end
   context 'a basic page' do
-    subject(:page) { ::Awestruct::Page.new '', [] }
+    subject(:page) { ::Awestruct::Page.new '', @site }
     it { should respond_to :front_matter }
     it { should respond_to :layout }
     it { should respond_to :render }
@@ -18,7 +23,20 @@ describe 'Awestruct::Page' do
     it { should respond_to :== }
   end
   context 'a page with front-matter' do
-    subject(:page) { Awestruct::Page.new 'test-data/front-matter-file-no-content.txt', [] }
-    it { page.front_matter.should eql [:foo => 'bar']}
+    subject(:page) { Awestruct::Page.new 'test-data/front-matter-file-no-content.txt', @site }
+    it { page.front_matter.should eql 'foo' => 'bar' }
+    it { page.source_path.should eql 'test-data/front-matter-file-no-content.txt' }
+  end
+  context 'a page with content' do
+    subject(:page) { Awestruct::Page.new 'test-data/simple-file.txt', @site }
+    it { page.raw_content.should_not be_nil }
+    it { page.raw_content.should eql "howdy\n" }
+    it { page.source_path.should eql 'test-data/simple-file.txt' }
+  end
+  context 'should accept a string as content' do
+    subject(:page) { Awestruct::Page.new 'testing', @site }
+    it { page.raw_content.should eql 'testing' }
+    it { page.source_path.should be_nil }
   end
 end
+
