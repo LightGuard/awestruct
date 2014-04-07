2c2d7e72a461c82d63e98bd2dd1d0ce1cc8bc961
LightGuard <lightguard.jp@gmail.com>
2014-04-07 09:51:04 -0600

    Partials are now reloaded

--- lib/awestruct/extensions/partial.rb
+++ lib/awestruct/extensions/partial.rb
@@ -11,13 +11,25 @@ module Awestruct
         end
 
         page = site.engine.load_site_page( filename )
-
         return nil if !page
 
         params.each do |k,v|
           page.send( "#{k}=", v )
         end if params
 
+        page.partial = true
+        from_site = site.partials.find {|p| p.source_path == page.source_path}
+
+        if from_site
+          from_site.dependencies.add_dependent self[:page]
+          self[:page].dependencies.add_dependency from_site
+          Awestruct::Dependencies.track_dependency(from_site)
+        else
+          page.dependencies.add_dependent self[:page]
+          self[:page].dependencies.add_dependency page
+          Awestruct::Dependencies.track_dependency(page)
+          site.partials << page
+        end
         page.content
       end
 
